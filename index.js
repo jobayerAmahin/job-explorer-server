fad4fddc04e325e3b0ff0c5cbae4ee41331fbdfe
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2025-04-28 23:47:01 +0300

    adding my job list and reinitializing coding

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.4hbs2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version SKF
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
